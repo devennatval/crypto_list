a6e8aa2fd3fabd4c669f2ab464ce8a316e2d15af
Deven Nathanael <devennatval@gmail.com>
2024-12-09 01:27:08 +0700

    Create TokenWithPrice and Currency Interface

---
+++ src/models/Currency.ts
@@ -0,0 +1,24 @@
+export interface CurrencyBase {
+    currency_id: number;
+    currencyGroup: string;
+    decimal_point: number;
+    logo: string;
+    listingDate: string;
+}
+
+export interface Wallet extends CurrencyBase {
+    tokenSymbol: string;
+    tokenType: string;
+    blockchain: string;
+    explorer: string;
+    blockchainName: string;
+}
+
+export interface Currency extends CurrencyBase {
+    currencySymbol: string;
+    name: string;
+    color: string;
+    wallets: Wallet[];
+    is_limit_order_enabled: boolean;
+    is_upcoming: boolean;
+}
---
+++ src/models/TokenWithPrice.ts
@@ -0,0 +1,8 @@
+export interface TokenWithPrice {
+    pair: string,
+    latestPrice: string,
+    day: string,
+    week: string,
+    month: string,
+    year: string
+}
