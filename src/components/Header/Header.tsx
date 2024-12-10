c6fb74aced57ef2f73866140b68ad808fec92e69
Deven Nathanael <devennatval@gmail.com>
2024-12-10 23:38:49 +0700

    Add Margin Right on Header Title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import SearchBar from "../SearchBar/SearchBar";
 
 const Header = () => {
     return <div className={"flex justify-between items-center mb-8"}>
-        <div className={"font-neue-haas text-xl md:text-2xl font-semibold pl-4 md:pl-0"}>
+        <div className={"font-neue-haas text-xl md:text-2xl font-semibold pl-4 md:pl-0 mr-4"}>
             Harga Crypto dalam Rupiah Hari Ini
         </div>
         <SearchBar/>
