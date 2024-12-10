5736ab85623d7d3b76f53c22257b30d5a1ddccf3
Deven Nathanael <devennatval@gmail.com>
2024-12-10 12:26:25 +0700

    Refactor Header Style and Add Margin

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import SearchBar from "../SearchBar/SearchBar";
 
 const Header = () => {
-    return <div className={"flex justify-between items-center"}>
+    return <div className={"flex justify-between items-center mb-8"}>
         <div className={"font-neue-haas text-xl md:text-2xl font-semibold pl-4 md:pl-0"}>
             Harga Crypto dalam Rupiah Hari Ini
         </div>
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,14 +3,14 @@ import { ReactSVG } from "react-svg";
 const SearchBar = () => {
     const icon = <ReactSVG src="/assets/icons/search.svg" width={20} height={20} />;
 
-    return <div>
-        <div className="hidden md:flex w-[22rem] items-center px-4 py-3 border border-secondary rounded-lg cursor-pointer transition hover:bg-dark-hover">
+    return <div className="cursor-pointer">
+        <div className="hidden md:flex w-[22rem] items-center px-4 py-3 border-dark transition hover:bg-dark-hover">
             { icon }
             <div className="ml-4 text-sm text-secondary">
                 Cari aset di Pintu
             </div>
         </div>
-        <div className="md:hidden px-4 py-2 cursor-pointer">
+        <div className="md:hidden px-4 py-2">
             { icon }
         </div>
     </div>
