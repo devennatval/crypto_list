9f09b705ff4803f7c7cf92ec6b84546b6d600681
Deven Nathanael <devennatval@gmail.com>
2024-12-10 16:55:52 +0700

    Add Bottom Margin on Tags Row

--- src/components/TagsRow/TagsRow.tsx
+++ src/components/TagsRow/TagsRow.tsx
@@ -4,13 +4,15 @@ import TagChip from "../TagChip/TagChip"
 const TagsRow = () => {
     const { marketTags } = useContent();
 
-    return <div className="flex overflow-x-auto space-x-2 py-2 hide-scrollbar">
-        {
-            marketTags.map(
-                ( tag, index ) => <TagChip name={tag.title} iconUrl={tag.icon.url} key={index}/>
-            )
-        }
-    </div>; 
+    return (
+        <div className="flex overflow-x-auto space-x-2 py-2 hide-scrollbar mb-6">
+            {
+                marketTags.map((tag) => (
+                    <TagChip key={"chip_tag_" + tag.slug} name={tag.title} iconUrl={tag.icon.url}/>
+                ))
+            }
+        </div>
+    );
 }
 
 export default TagsRow;
