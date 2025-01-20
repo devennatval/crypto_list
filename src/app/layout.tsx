8e536811efad2e3c3cf8dcc9ac8d185a4e095b03
Deven Nathanael <65056355+devennatval@users.noreply.github.com>
2025-01-20 19:14:26 +0700

    Change Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "PINTU ASSESSMENT DEVEN",
+  title: "Crypto List",
 };
 
 export default function RootLayout({
